d69e894b1bafe228eeb15446cc92431b675bfc41
laurens <lnkuiper@github.com>
2020-06-11 09:43:56 +0000

    push to local

--- src/main/client_context.cpp
+++ src/main/client_context.cpp
@@ -189,7 +189,7 @@ unique_ptr<PreparedStatementData> ClientContext::CreatePreparedStatement(const s
 	
 	if (enable_reoptimizer && plan->type == LogicalOperatorType::PREPARE && !subquery) {
 		ReOptimizer reoptimizer = ReOptimizer(*this, planner.binder);
-		plan = reoptimizer.SimulatedReOptimizeCost(move(plan), query, 1.2);
+		plan = reoptimizer.SimulatedReOptimizeCost(move(plan), query, 1.15);
 		assert(plan);
 	}
 
